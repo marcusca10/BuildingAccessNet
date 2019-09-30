4df63c99050712555c280df9394148782ca1e2fc
Marcus Carvalho <marcusca@microsoft.com>
2019-09-30 06:31:33 +0100

    Included DB project

---
+++ buildingaccess-db/dbo/Tables/Tenants.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Tenants] (
+    [Id]              UNIQUEIDENTIFIER NOT NULL,
+    [Name]            NVARCHAR (MAX)   NULL,
+    [Caption]         NVARCHAR (MAX)   NULL,
+    [MetadataAddress] NVARCHAR (MAX)   NULL,
+    [Realm]           NVARCHAR (MAX)   NULL,
+    [Owner]           NVARCHAR (MAX)   NULL,
+    CONSTRAINT [PK_dbo.Tenants] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
